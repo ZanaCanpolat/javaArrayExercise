f55bcf1a91bfc2876e19c7c7b77adb00f66c065d
Zana Canpolat <zana_canpolat@hotmail.se>
2025-01-20 14:53:01 +0100

    Create an array of integers and print them out

--- src/main/java/org/Lexicon/group54/ArrayExercise.java
+++ src/main/java/org/Lexicon/group54/ArrayExercise.java
@@ -1,7 +1,30 @@
 package org.Lexicon.group54;
 
-public class ArrayExercise {
+import java.util.Arrays;
 
-    //
+public class ArrayExercise
+{
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("----------------- Array Exercise 1 ----------------- ");
+    // Write a program which will store elements in an array of type ‘int’ and print it out.
+        int [] arrayOfIntegers = {9, 1, 7, 90, 3, 99};
+        System.out.println("Array Of Integers = " + Arrays.toString(arrayOfIntegers));
+
+
+        System.out.println("----------------- Array Exercise 2 ----------------- ");
+        
+
+
+    }
+
+
+
+
+
+
+
 
 }

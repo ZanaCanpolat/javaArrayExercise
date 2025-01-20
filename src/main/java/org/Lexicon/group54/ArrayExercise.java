29492a4e76399715b4849182f2b4d4cf5ec8a0ab
Zana Canpolat <zana_canpolat@hotmail.se>
2025-01-20 15:11:05 +0100

    Created a method that searchs throw an array if element found returns element index otherwise -1

--- src/main/java/org/Lexicon/group54/ArrayExercise.java
+++ src/main/java/org/Lexicon/group54/ArrayExercise.java
@@ -8,19 +8,41 @@ public class ArrayExercise
     public static void main(String[] args)
     {
 
+         // Write a program which will store elements in an array of type ‘int’ and print it out.
         System.out.println("----------------- Array Exercise 1 ----------------- ");
-    // Write a program which will store elements in an array of type ‘int’ and print it out.
         int [] arrayOfIntegers = {9, 1, 7, 90, 3, 99};
         System.out.println("Array Of Integers = " + Arrays.toString(arrayOfIntegers));
+        System.out.println();
 
 
+        //Create a program and create a method with name ‘indexOf’ which will  find and return the index of an
+        // element in the array. If the element doesn’t exist your method should return -1 as value.
+        // If the element is found, position is printed otherwise -1 is printed.
         System.out.println("----------------- Array Exercise 2 ----------------- ");
-        
-
+        int numberToFind = 900;
+        if (indexOf(arrayOfIntegers, numberToFind) == -1)
+        {
+            System.out.println("Nr " +numberToFind +" is not in the array.");
+        }
+        else{
+        System.out.println("Nr " +numberToFind +" is at the position: " +(indexOf(arrayOfIntegers, 90)));
+        }
 
     }
 
-
+    //This method will find and return the index of an element in the array.
+    // If the element doesn’t exist your method should return -1 as value.
+    public static float indexOf(int[] array, int element)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            if (array[i] == element)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 
 
